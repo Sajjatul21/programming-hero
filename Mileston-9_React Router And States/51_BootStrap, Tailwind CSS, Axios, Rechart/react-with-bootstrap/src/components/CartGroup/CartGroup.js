f0882e5e52b5eb224872d2495f4dac0d35dc8707
Sajjatul21 <76829263+Sajjatul21@users.noreply.github.com>
2023-06-02 00:47:10 +0100

    this is raect thats why we are not usiging same html code we crete cart component and also create a products array  then pass the array on cart component

--- Mileston-9_React Router And States/51_BootStrap, Tailwind CSS, Axios, Rechart/react-with-bootstrap/src/components/CartGroup/CartGroup.js
+++ Mileston-9_React Router And States/51_BootStrap, Tailwind CSS, Axios, Rechart/react-with-bootstrap/src/components/CartGroup/CartGroup.js
@@ -1,32 +1,18 @@
 import React from 'react';
+import Cart from '../Cart/Cart';
 
 const CartGroup = () => {
+    const products = [
+        { id: 1, name: 'Mac Book Air m2', price: 200000 },
+        { id: 2, name: 'Mac Book pro', price: 150000 },
+        { id: 3, name: 'Mac Book pro max', price: 155000 },
+    ];
     return (
         <div className="card-group">
-            <div className="card">
-                <img src="..." className="card-img-top" alt="..." />
-                <div className="card-body">
-                    <h5 className="card-title">Card title</h5>
-                    <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                    <p className="card-text"><small className="text-body-secondary">Last updated 3 mins ago</small></p>
-                </div>
-            </div>
-            <div className="card">
-                <img src="..." className="card-img-top" alt="..." />
-                <div className="card-body">
-                    <h5 className="card-title">Card title</h5>
-                    <p className="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
-                    <p className="card-text"><small className="text-body-secondary">Last updated 3 mins ago</small></p>
-                </div>
-            </div>
-            <div className="card">
-                <img src="..." className="card-img-top" alt="..." />
-                <div className="card-body">
-                    <h5 className="card-title">Card title</h5>
-                    <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
-                    <p className="card-text"><small className="text-body-secondary">Last updated 3 mins ago</small></p>
-                </div>
-            </div>
+
+            {
+                products.map(product => <Cart key={product.id} product={product}></Cart>)
+            }
         </div>
     );
 };
