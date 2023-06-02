86fbf21dd02a08bbab4d46382009c5851ab39173
Sajjatul21 <76829263+Sajjatul21@users.noreply.github.com>
2023-06-02 15:29:52 +0100

    tailwind css added

--- Mileston-9_React Router And States/51-3 Setup tailwind css with Create React App/price-club/src/App.jsx
+++ Mileston-9_React Router And States/51-3 Setup tailwind css with Create React App/price-club/src/App.jsx
@@ -1,14 +1,12 @@
-
 import './App.css';
-import NavBar from './components/NavBar/NavBar';
 
 function App() {
 
+
   return (
-    <>
-      <NavBar></NavBar>
-      <h1 className='text-4xl font-bold underline text-center'>This is big big header</h1>
-    </>
+    <div>
+      <h1 className='text-4xl font-bold underline'>This is a big big headder</h1>
+    </div>
   );
 }
 
