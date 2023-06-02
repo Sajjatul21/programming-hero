976ff50b3179ed1b273431af531b59cc77896010
Sajjatul21 <76829263+Sajjatul21@users.noreply.github.com>
2023-06-02 01:32:46 +0100

    spinner added

--- Mileston-9_React Router And States/51-2_Install React Bootstrap in your reack application/explore-react-bootstrap/src/App.js
+++ Mileston-9_React Router And States/51-2_Install React Bootstrap in your reack application/explore-react-bootstrap/src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
-import { Button } from 'react-bootstrap';
+import { Button, Spinner } from 'react-bootstrap';
 
 function App() {
   return (
     <div className="App">
       <button>HTML Button</button>
       <Button variant='danger'>React Button</Button>
+      <br />
+      <Spinner animation="grow" variant="danger" />
 
     </div>
   );
