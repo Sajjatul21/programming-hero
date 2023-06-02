ed2af6f9e2b4e47860c7b3d057f48f03932dd849
Sajjatul21 <76829263+Sajjatul21@users.noreply.github.com>
2023-06-02 01:11:16 +0100

    go this site using bootstrap on react

---
+++ Mileston-9_React Router And States/51-2_Install React Bootstrap in your reack application/explore-react-bootstrap/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      {/* got to this site : https://react-bootstrap.github.io/" */}
+    </div>
+  );
+}
+
+export default App;
