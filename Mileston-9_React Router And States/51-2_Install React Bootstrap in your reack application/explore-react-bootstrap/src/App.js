1f8d2a1b1bb3868d2785444d06a6e61465a41041
Sajjatul21 <76829263+Sajjatul21@users.noreply.github.com>
2023-06-02 01:29:15 +0100

    react btn addded

--- Mileston-9_React Router And States/51-2_Install React Bootstrap in your reack application/explore-react-bootstrap/src/App.js
+++ Mileston-9_React Router And States/51-2_Install React Bootstrap in your reack application/explore-react-bootstrap/src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import { Button } from 'react-bootstrap';
 
 function App() {
   return (
     <div className="App">
-      
-   
+      <button>HTML Button</button>
+      <Button>React Button</Button>
+
     </div>
   );
 }
