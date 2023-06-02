0878f759d0c86634c083826698bb2efefaee219b
Sajjatul21 <76829263+Sajjatul21@users.noreply.github.com>
2023-06-02 04:02:25 +0100

    navvar added

---
+++ Mileston-9_React Router And States/51-3 Setup tailwind css with Create React App/price-club/src/components/NavBar/NavBar.js
@@ -0,0 +1,17 @@
+
+const NavBar = () => {
+    const routes = [
+        { id: 1, name: "Home", path: "/home" },
+        { id: 2, name: "Products", path: "/products" },
+        { id: 3, name: "Orders", path: "/Orders" },
+        { id: 4, name: "Contact", path: "/contact" },
+        { id: 5, name: "About", path: "/about" },
+    ];
+    return (
+        <nav>
+
+        </nav>
+    );
+};
+
+export default NavBar;
