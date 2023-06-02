2b697ccc7da93e38790e02417d41b09ec6bd4700
Sajjatul21 <76829263+Sajjatul21@users.noreply.github.com>
2023-06-02 17:07:02 +0100

    create LIink

---
+++ Mileston-9_React Router And States/51-3 Setup tailwind css with Create React App/price-club/src/components/NavBar/NavBar.js
@@ -0,0 +1,20 @@
+import Link from "../Link/Link";
+
+const NavBar = () => {
+    const routes = [
+        { id: 1, name: "Home", Path: "/home" },
+        { id: 2, name: "Products", Path: "/home" },
+        { id: 3, name: "Orders", Path: "/home" },
+        { id: 4, name: "Contact", Path: "/home" },
+        { id: 5, name: "About", Path: "/home" },
+    ];
+    return (
+        <div>
+            {
+                routes.map(route=><Link key={route.id} route></Link>)
+            }
+        </div>
+    );
+};
+
+export default NavBar;
