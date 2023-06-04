de59b72daf06ffd5e97089652ada8243cbf583b6
Sajjatul21 <76829263+Sajjatul21@users.noreply.github.com>
2023-06-04 08:31:35 +0100

    phones give an opject. defend on phones object we using rechart

--- Mileston-9_React Router And States/51-3 Setup tailwind css with Create React App/price-club/src/components/PhoneBar/PhoneBar.jsx
+++ Mileston-9_React Router And States/51-3 Setup tailwind css with Create React App/price-club/src/components/PhoneBar/PhoneBar.jsx
@@ -1,12 +1,10 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
+import { Bar, BarChart, Tooltip, XAxis, YAxis } from "recharts";
 
 const PhoneBar = () => {
     const [phones, setPhones] = useState([]);
     useEffect(() => {
-        /*  fetch('https://openapi.programming-hero.com/api/phones?search=iphone')
-             .then(res => res.json())
-             .then(data => console.log(data)); */
 
         axios.get('https://openapi.programming-hero.com/api/phones?search=iphone')
             .then(data => {
@@ -22,14 +20,18 @@ const PhoneBar = () => {
                     };
                     return singlePhone;
                 });
-                console.log((phoneData));
+                setPhones(phoneData);
+
             });
 
     }, []);
     return (
-        <div>
-
-        </div>
+        <BarChart width={500} height={200} data={phones}>
+            <Bar dataKey="price" fill="#8884d8" />
+            <XAxis dataKey={name} />
+            <YAxis></YAxis>
+            <Tooltip></Tooltip>
+        </BarChart>
     );
 };
 
