f87710b76a59b9616a4b5ab360ad5170b2461b64
Sajjatul21 <76829263+Sajjatul21@users.noreply.github.com>
2023-06-04 08:12:46 +0100

    use axios

--- Mileston-9_React Router And States/51-3 Setup tailwind css with Create React App/price-club/src/App.jsx
+++ Mileston-9_React Router And States/51-3 Setup tailwind css with Create React App/price-club/src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css';
 import AssiignmentMarks from './components/AssiignmentMarks/AssiignmentMarks';
 import NavBar from './components/NavBar/NavBar';
+import PhoneBar from './components/PhoneBar/PhoneBar';
 import Pricing from './components/Pricing/Pricing';
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
       <h1 className='text-4xl font-bold underline mt-2'>This is a big big headder</h1>
       <Pricing></Pricing>
       <AssiignmentMarks></AssiignmentMarks>
+      <PhoneBar></PhoneBar>
     </div>
   );
 }
--- Mileston-9_React Router And States/51-3 Setup tailwind css with Create React App/price-club/src/components/PhoneBar/PhoneBar.jsx
+++ Mileston-9_React Router And States/51-3 Setup tailwind css with Create React App/price-club/src/components/PhoneBar/PhoneBar.jsx
@@ -1,8 +1,24 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 const PhoneBar = () => {
+    const [phones, setPhones] = useState([]);
+    useEffect(() => {
+        /*  fetch('https://openapi.programming-hero.com/api/phones?search=iphone')
+             .then(res => res.json())
+             .then(data => console.log(data)); */
+
+        axios.get('https://openapi.programming-hero.com/api/phones?search=iphone')
+            .then(data => {
+                // console.log(data)
+                const phones = data.data.data;
+                console.log(phones);
+            });
+
+    }, []);
     return (
         <div>
-                {/*phero-reposotory-broken-phne-> https://openapi.programming-hero.com/api/phones?search=iphone */}
+
         </div>
     );
 };
