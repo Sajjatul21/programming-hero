6e1b5b6bb1959c42f2a00f6f97d301c8d369dce6
Sajjatul21 <76829263+Sajjatul21@users.noreply.github.com>
2023-06-04 08:01:42 +0100

    api link

---
+++ Mileston-9_React Router And States/51-3 Setup tailwind css with Create React App/price-club/src/components/PhoneBar/PhoneBar.jsx
@@ -0,0 +1,10 @@
+
+const PhoneBar = () => {
+    return (
+        <div>
+                {/*phero-reposotory-broken-phne-> https://openapi.programming-hero.com/api/phones?search=iphone */}
+        </div>
+    );
+};
+
+export default PhoneBar;
