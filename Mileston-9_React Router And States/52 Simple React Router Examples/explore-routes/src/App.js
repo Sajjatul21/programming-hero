7bee5d0fdc416fdf534f36c23c815839742404bf
Sajjatul21 <76829263+Sajjatul21@users.noreply.github.com>
2023-06-10 04:58:22 +0100

    52-1 Module Introduction, simple react router setup

---
+++ Mileston-9_React Router And States/52 Simple React Router Examples/explore-routes/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <h1>52-1 Module Introduction, simple react router setup</h1> */}
+    </div>
+  );
+}
+
+export default App;
